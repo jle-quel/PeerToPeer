121e30eb0c132e42353d964849b033bd8fdc4a2c
Jefferson <jeffersonlequellec@me.com>
2017-12-27 13:28:28 +0100

    Still working on the getAddr()

---
+++ header.go
@@ -0,0 +1,69 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   header.go                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: Jefferso <Jefferso@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/12/26 20:55:13 by Jefferso          #+#    #+#             */
+/*   Updated: 2017/12/27 13:27:02 by Jefferson        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+package main
+
+import (
+	"os/exec"
+	"os"
+	"net"
+	"fmt"
+)
+
+const USHRT_MAX = 65535
+
+type s_broadcast struct {
+	guid string
+	hash uint16
+	addr string
+}
+
+/* ************************************************************************** */
+
+func getGuid() string {
+	cmd := exec.Command("sh", "-c", "/usr/bin/base64 /dev/random | /usr/bin/head -c 64")
+	out, _ := cmd.Output()
+	return (string(out))
+}
+
+func getHash(str string) uint16 {
+	var ret	uint16
+
+	ret = 0
+	for _, value := range str {
+		ret += uint16(value)
+		ret += (ret << 10)
+		ret ^= (ret >> 6)
+	}
+	ret += (ret << 3)
+	ret ^= (ret >> 11)
+	ret += (ret << 15)
+	ret = (ret % USHRT_MAX)
+	return ret
+}
+
+func getAddr() {
+	host, err := os.Hostname()
+	handleErr(err)
+	addrs, err := net.LookupIP(host)
+	handleErr(err)
+
+	for _, value := range addrs {
+		fmt.Println(value)
+	}
+}
+
+func getHeader() s_broadcast {
+	guid := getGuid()
+	header := s_broadcast{guid, getHash(guid), "lol"}
+	return header
+}
--- tools.go
+++ tools.go
@@ -1,29 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.go                                            :+:      :+:    :+:   */
+/*   tools.go                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: Jefferso <Jefferso@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/12/26 20:55:08 by Jefferso          #+#    #+#             */
-/*   Updated: 2017/12/27 13:22:50 by Jefferson        ###   ########.fr       */
+/*   Created: 2017/12/26 21:36:30 by Jefferso          #+#    #+#             */
+/*   Updated: 2017/12/26 21:45:16 by Jefferson        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
-/* ************************************************************************** */
-
-func	main() {
-	var header	s_broadcast
-
-	// CONFLICT WITH THE HASH
-	header = getHeader()
-	fmt.Println("Guid ->", header.guid)
-	fmt.Println("Hash ->", header.hash)
-	fmt.Println("Addr ->", header.addr)
-	fmt.Printf("\n")
-	getAddr()
+func	handleErr(err error) {
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }
